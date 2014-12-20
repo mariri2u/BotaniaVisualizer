b085c86b30ad7b9d925eafc423a37f576667dc2b
mariri2u <github@marur.info>
2014-12-20 10:53:41 +0900

    Ver1.0aリリース
    テクスチャ変更

--- src/java/mariri/botaniavisualizer/BotaniaVisualizer.java
+++ src/java/mariri/botaniavisualizer/BotaniaVisualizer.java
@@ -15,7 +15,7 @@ import cpw.mods.fml.common.registry.GameRegistry;
 @Mod(modid = BotaniaVisualizer.MODID, version = BotaniaVisualizer.VERSION, dependencies = BotaniaVisualizer.DEPENDENCIES )
 public class BotaniaVisualizer {
     public static final String MODID = "BotaniaVisualizer";
-    public static final String VERSION = "1.7.2-1.0";
+    public static final String VERSION = "1.7.2-1.0a";
     public static final String DEPENDENCIES = "required-after:Botania";
     private Item itemManaReader; 
     
